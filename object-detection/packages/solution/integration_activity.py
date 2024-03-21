1ee60a5f0c36961f0b8f6daab3804e606475231b
Thomson <21tpac@queensu.ca>
2024-03-20 23:06:03 -0400

    group 9, collision avoidance

--- object-detection/packages/solution/integration_activity.py
+++ object-detection/packages/solution/integration_activity.py
@@ -3,7 +3,7 @@ from typing import Tuple
 
 def DT_TOKEN() -> str:
     # TODO: change this to your duckietown token
-    dt_token = "PUT_YOUR_TOKEN_HERE"
+    dt_token = "dt1-3nT7FDbT7NLPrXykNJmqqgqJCKUj6tgA6MrBnrpiCsAk1y6-43dzqWFnWd8KBa1yev1g3UKnzVxZkkTbfZHC69Yzhqajb8opKTFNBNZVvphoVEXG5y"
     return dt_token
 
 
@@ -16,7 +16,7 @@ def MODEL_NAME() -> str:
 def NUMBER_FRAMES_SKIPPED() -> int:
     # TODO: change this number to drop more frames
     # (must be a positive integer)
-    return 0
+    return 1
 
 
 def filter_by_classes(pred_class: int) -> bool:
@@ -34,10 +34,15 @@ def filter_by_classes(pred_class: int) -> bool:
     Args:
         pred_class: the class of a prediction
     """
+    if pred_class == 0:
+        return True
+    else:
+        return False
+
     # Right now, this returns True for every object's class
     # TODO: Change this to only return True for duckies!
     # In other words, returning False means that this prediction is ignored.
-    return True
+    
 
 
 def filter_by_scores(score: float) -> bool:
@@ -45,10 +50,14 @@ def filter_by_scores(score: float) -> bool:
     Args:
         score: the confidence score of a prediction
     """
+    if score < 0.15:
+        return False
+    else:
+        return True
+
     # Right now, this returns True for every object's confidence
     # TODO: Change this to filter the scores, or not at all
     # (returning True for all of them might be the right thing to do!)
-    return True
 
 
 def filter_by_bboxes(bbox: Tuple[int, int, int, int]) -> bool:
@@ -57,5 +66,11 @@ def filter_by_bboxes(bbox: Tuple[int, int, int, int]) -> bool:
         bbox: is the bounding box of a prediction, in xyxy format
                 This means the shape of bbox is (leftmost x pixel, topmost y, rightmost x, bottommost y)
     """
+    x1, y1, x2, y2 = bbox
+    if y2>350:
+        return True
+    else:
+        return False
+
     # TODO: Like in the other cases, return False if the bbox should not be considered.
-    return True
+   
